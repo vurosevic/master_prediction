25a2516e6fa4ea6d8a5f8509a1d6ea8ba8d6746f
vurosevic <35973264+vurosevic@users.noreply.github.com>
2018-10-06 14:07:53 +0200

    start

--- src/master_prediction/data.clj
+++ src/master_prediction/data.clj
@@ -24,25 +24,6 @@ master-prediction.data
         (map #(map parse-float %) d)
         (map (fn [s] {:x  (vec (drop-last s))  :y (last s)}) d)))
 
-(defn read-data-training
-  "Read the csv file, split out each line and then each number, parse the tokens and break up the numbers so that the last is the target and everything else is the feature vector."
-  []
-  (as-> (slurp "resources/data_trening.csv") d
-        (string/split d #"\n")
-        (map #(string/split % #",") d)
-        (map #(map parse-float %) d)
-        (map (fn [s] {:x  (vec (drop-last s))  :y (last s)}) d)))
-
-(defn read-data-test
-  "Read the csv file, split out each line and then each number, parse the tokens and break up the numbers so that the last is the target and everything else is the feature vector."
-  []
-  (as-> (slurp "resources/data_test.csv") d
-        (string/split d #"\n")
-        (map #(string/split % #",") d)
-        (map #(map parse-float %) d)
-        (map (fn [s] {:x  (vec (drop-last s))  :y (last s)}) d)))
-
-
 (defn write-file [filename data]
   (with-open [w (clojure.java.io/writer  (str "resources/" filename) :append true)]
     (.write w data)))
@@ -178,16 +159,6 @@ master-prediction.data
     (axpy! min-vector (mul norm-vector maxmin-vector))
     )
   )
-
-
-
-;; matrixs for training, 70% of all data
-(def input_matrix2 (dge 50 276 (reduce into [] (map :x (read-data-training)))))
-(def target_matrix2 (dge 1 276 (map :y (read-data-training))))
-
-;; matrixs for test, 30% of all data
-(def input_test_matrix2 (dge 50 91 (reduce into [] (map :x (read-data-test)))))
-(def target_test_matrix2 (dge 1 91 (map :y (read-data-test))))
 
 ;; matrix with new data
 (def input-matrix-all (dge 64 2647 (reduce into [] (map :x (read-data-from-csv "resources/data_prediction.csv")))))
